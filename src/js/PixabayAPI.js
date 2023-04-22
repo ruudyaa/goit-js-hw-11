8e7e750f5a5087c9ff9e32c7d4f0418009437a10
ruudyaa <martarudnytska26@gmail.com>
2023-04-22 03:44:55 +0300

    Adds PixabayAPI class.

---
+++ src/js/PixabayAPI.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '35648673-0e16e427271b01d9e7b771895';
+
+export class PixabayAPI {
+  #page = 1;
+  #per_page = 40;
+  #query = '';
+  #totalPages = 0;
+
+  async getPhotos() {
+    const params = {
+      page: this.#page,
+      q: this.#query,
+      per_page: this.#per_page,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    };
+
+    const urlAXIOS = `?key=${API_KEY}`;
+
+    const { data } = await axios.get(urlAXIOS, { params });
+    return data;
+  }
+
+  get query() {
+    this.#query;
+  }
+
+  set query(newQuery) {
+    this.#query = newQuery;
+  }
+
+  incrementPage() {
+    this.#page += 1;
+  }
+
+  resetPage() {
+    this.#page = 1;
+  }
+
+  setTotal(total) {
+    this.#totalPages = total;
+  }
+
+  hasMorePhotos() {
+    return this.#page < Math.ceil(this.#totalPages / this.#per_page);
+  }
+}
