8b814d237e9c9c78fe88f57abc580f39875f233f
ruudyaa <martarudnytska26@gmail.com>
2023-04-22 03:44:05 +0300

    Adds separate function for markup creation.

---
+++ src/js/createMarkup.js
@@ -0,0 +1,42 @@
+export function createMarkup(photos) {
+  return photos
+    .map(
+      ({
+        tags,
+        webformatURL,
+        largeImageURL,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+            <a href='${largeImageURL}' class="card-link js-card-link">
+            <div class="photo-card">
+              <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+              <div class="info">
+                <div class="info-item info-item-likes">
+                  <div class="box-likes"><b>Likes</b>
+                  <span id="value">${likes}</span>
+                  </div>
+                  
+                </div>
+                <p class="info-item">
+                  <b>Views</b>
+                  ${views}
+                </p>
+                <p class="info-item">
+                  <b>Comments</b>
+                  ${comments}
+                </p>
+                <p class="info-item">
+                  <b>Downloads</b>
+                  ${downloads}
+                </p>
+              </div>
+            </div>
+            </a>`;
+      }
+    )
+    .join('');
+}
