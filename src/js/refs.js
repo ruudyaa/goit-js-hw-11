148e4f602e09ad8a6b9abbc0ef7c1ca37900a237
ruudyaa <martarudnytska26@gmail.com>
2023-04-22 03:45:10 +0300

    Adds references for components.

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+export const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  btnLoadMore: document.querySelector('.load-more'),
+  backdrop: document.querySelector('.backdrop'),
+  spinner: document.querySelector('.js-spinner'),
+  body: document.querySelector('body'),
+  like: document.querySelector('.heart'),
+  searchInput: document.querySelector('.search-form-input'),
+};
